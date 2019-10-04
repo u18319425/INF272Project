0131ac77a6f754e681a5e4e82ca7f5d39a47e5d1
OnkeMweza <56151334+OnkeMweza@users.noreply.github.com>
2019-10-04 17:46:40 +0200

    Adding project to repository

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272Project.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult WelcomePage()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult MedicalInfo()
+        {
+            return View();
+        }
+
+        public ActionResult PatientDetails()
+        {
+            return View();
+        }
+
+        public ActionResult UpdatePatientDetails()
+        {
+            return View();
+        }
+
+        public ActionResult CreatePatientDetails()
+        {
+            return View();
+        }
+
+        public ActionResult CreateMedicalInfo()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateMedicalInfo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Deliverable2.MvcApplication" Language="C#" %>
