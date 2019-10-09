18a5c54e1e871a081ec7e87ecda5bd2fea84a2f8
OnkeMweza <56151334+OnkeMweza@users.noreply.github.com>
2019-10-09 23:42:55 +0200

    New Function Added

---
+++ Scripts/JavaScript.js
@@ -0,0 +1,16 @@
+﻿function Vitals()
+{
+   
+    document.getElementById("hRate").innerHTML = Math.floor((Math.random() * 100) + 80);
+    document.getElementById("bSugar").innerHTML = Math.floor((Math.random() * 170) + 60);
+    document.getElementById("wBlood").innerHTML = Math.floor((Math.random() * 100000) + 30000);
+    document.getElementById("rBlood").innerHTML = Math.floor((Math.random() * 100000) + 30000);
+    document.getElementById("glucose").innerHTML = Math.floor((Math.random() * 140) + 50);
+    document.getElementById("cholesterol").innerHTML = Math.floor((Math.random() * 100) + 20);
+    document.getElementById("bPressure").innerHTML = Math.floor((Math.random() * 150) + 35);
+    document.getElementById("antibodies").innerHTML = Math.floor((Math.random() * 20000) + 7000);
+    document.getElementById("iLevels").innerHTML = Math.floor((Math.random() * 600) + 450);
+    
+}
+
+setInterval(Vitals(), 2000);
